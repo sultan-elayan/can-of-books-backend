c5b82e02a817406cbd6a74058297a7b5c6840fdd
mayyadah <miss.mayadah1@gmail.com>
2021-07-05 23:04:49 +0300

    edit userSchema

--- models/Book.model.js
+++ models/Book.model.js
@@ -4,9 +4,9 @@ const mongoose=require('mongoose');
 
 
 const bookSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  status: String,
+  name: {type:String},
+  description: {type:String},
+  status: {type:String},
 
 });
 
