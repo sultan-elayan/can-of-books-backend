0f06dbfadc8ea52c0f7ed65a54a7ece2db9a35fd
mayyadah <miss.mayadah1@gmail.com>
2021-07-05 17:38:53 +0300

    start mongo

---
+++ models/Book.model.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const mongoose=require('mongoose');
+
+
+const usersSchema = new mongoose.Schema({
+    email: String,
+    books: [bookSchema],
+  });
+
+  module.exports=usersSchema
